4ad5357e15bd5eaa31e1073bb99e4ff2c9c92f47
Erik de Vries <erik@devries.pm>
2018-12-11 11:02:19 +0100

    elasticizer: Added 900s timeout after every batch of 200000 articles when updating, to allow ES to do some segment merges (and clean up disk space)

--- R/elasticizer.R
+++ R/elasticizer.R
@@ -64,6 +64,9 @@ elasticizer <- function(query, src = T, index = 'maml', es_pwd = .rs.askForPassw
         if (length(update) > 0){
           out <-  jsonlite:::flatten(json$hits$hits)
           update(out, localhost = localhost, ...)
+          if (batch%%200 == 0) {
+            Sys.sleep(900)
+          }
         } else {
           out <- bind_rows(out, jsonlite:::flatten(json$hits$hits))
         }
