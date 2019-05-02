2fc48cc2f7c29ca90315f388f7ff5f1156ab784a
Erik de Vries <erik@devries.pm>
2019-05-02 13:10:32 +0200

    dfm_gen: fixed absence of out$codes field

--- R/dfm_gen.R
+++ R/dfm_gen.R
@@ -32,13 +32,13 @@ dfm_gen <- function(out, words = '999', text = "lemmas", clean, cores = detectCo
   if ('_source.codes.majorTopic' %in% colnames(out)) {
     out <- out %>%
       mutate(junk = case_when(
-          .$codes == 92 ~ 1,
-          .$codes == 91 ~ 1,
-          .$codes == 93 ~ 1,
+          .$`_source.codes.majorTopic` == 92 ~ 1,
+          .$`_source.codes.majorTopic` == 91 ~ 1,
+          .$`_source.codes.majorTopic` == 93 ~ 1,
           TRUE ~ 0
         )
       ) %>%
-      mutate(aggregate = .$codes %>%
+      mutate(aggregate = .$`_source.codes.majorTopic` %>%
                str_pad(4, side="right", pad="a") %>%
                str_match("([0-9]{1,2})?[0|a][1-9|a]") %>%
                .[,2] %>%
