0e593075eebdab68c9dcaa5ee54d96e9cad49c2a
Your Name <you@example.com>
2020-06-15 19:04:26 +0200

    query_gen_actors: only retrieve ud field from source

--- R/query_gen_actors.R
+++ R/query_gen_actors.R
@@ -15,7 +15,8 @@
 #################################################################################################
 query_gen_actors <- function(actor, country, pre_tags, post_tags) {
   generator <- function(country, startdate, enddate, querystring, pre_tags, post_tags, actorid) {
-    return(paste0('{"query":
+    return(paste0('{"_source": ["ud"],
+    "query":
                     {"bool": {
                       "filter":[
                         {"term":{"country":"',country,'"}},
