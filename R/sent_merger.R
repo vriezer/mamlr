108372452cd5aefa6d3f3de3d7b78fe2cbd25388
Erik de Vries <erik.devries@uis.no>
2022-01-25 18:39:27 +0100

    sent_merger: Correctly add party metadata for _mfsa aggregations

--- R/sent_merger.R
+++ R/sent_merger.R
@@ -201,7 +201,7 @@ sent_merger <- function(df, actors_meta = NULL, actor_groups = NULL, pos_cutoff 
       publication_date = first(publication_date)
     ), by = c('id','partyId')] # Summarize by article and partyId
     # Add party metadata
-    parties_actors <- actors_meta[parties_actors, on = c('partyId'), mult = 'first'][!is.na(id),.(ids = str_c(ids,"_mfsa"), (.SD)), .SDcols = -c('ids')]
+    parties_actors2 <- actors_meta[str_starts(ids, 'P_')][parties_actors, on = c('partyId'), mult = 'first'][!is.na(id),.(ids = str_c(partyId,"_mfsa"), (.SD)), .SDcols = -c('ids')]
 
     ## Generate party aggregations (mfs)
     parties <- df[str_ends(ids,'_f') | str_ends(ids,'_s'),.(
